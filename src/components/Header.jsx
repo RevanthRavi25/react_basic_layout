965f55ff3163d79999ae54a2393be9c2415c4d8e
Revanth R. <rr13888@ISOLVEGROUP.ORG>
2024-05-06 12:27:49 +0530

    header change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = () => {
     return (
         <header>
-            <h1>Header</h1>
-        </header>
+            <h1>Header main</h1>
+        </header>    
     );
 };
 
